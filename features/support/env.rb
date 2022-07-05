9b3bd897ad0820938046dc48a7ed11296f5ab832
Rashika <agarwalrashika00@gmail.com>
2022-07-05 12:44:21 +0530

    added hooks

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,5 +2,11 @@ require "rubygems"
 require "selenium-webdriver"
 require "test-unit"
 
-driver = Selenium::WebDriver.for :chrome
-$driver = driver
+Before do |scenario|
+    driver = Selenium::WebDriver.for :chrome
+    $driver = driver
+end
+
+After do |scenario|
+    $driver.close
+end
