8c4321a989c534b88badb2af14c5fae5ca2c9029
Rashika <agarwalrashika00@gmail.com>
2022-07-09 12:58:23 +0530

    used expect to match title

--- features/library/page_object/home_page.rb
+++
@@ -1,9 +0,0 @@
-class Homepage < Test::Unit::TestCase
-    def setup
-        @driver = $driver
-    end
-
-    def verify_title
-        assert_equal('Facebook - log in or sign up', @driver.title)
-    end
-end
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -2,9 +2,9 @@ Given(/^I am on facebook homepage$/) do
     $driver.navigate.to "https://www.facebook.com/"
     puts "I am on facebook home page"
     sleep 2
-
 end
+
 Then(/^Match title of the page$/) do
-   Homepage
-   puts "Title of homepage verified"
+    expect($driver.title.start_with? 'Facebook').to eq(true)
+    puts "Title of homepage verified"
 end
